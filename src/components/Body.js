8cc3e46cd0e27ec6690eb69793bd6c8b40603d3f
UdayGodaba <godabauday@gmail.com>
2024-02-14 12:17:14 +0530

    Performance improvement using Loading on Demand concept

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Login from "./Login";
-import Browse from "./Browse";
+// import Browse from "./Browse";
 
 const Body = () => {
+  const Browse = lazy(() => import("./Browse"));
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -12,7 +14,11 @@ const Body = () => {
     },
     {
       path: "/browse",
-      element: <Browse />,
+      element: (
+        <Suspense fallback={<h1>Loading</h1>}>
+          <Browse />
+        </Suspense>
+      ),
     },
   ]);
 
